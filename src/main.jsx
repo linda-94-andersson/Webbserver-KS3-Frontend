f95b4c27a64d1b876d3e7a3bb491544a093613a0
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-30 18:35:40 +0200

    Added RecoilRoot

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-import React, {Suspense} from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import "./index.css";
 
 const App = React.lazy(() => import("./App"));
@@ -10,7 +11,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Suspense fallback={<div>Loading...</div>}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" exact element={<App />} />
+          <Route
+            path="/"
+            exact
+            element={
+              <RecoilRoot>
+                <App />{" "}
+              </RecoilRoot>
+            }
+          />
           <Route>404 Not Found!</Route>
         </Routes>
       </BrowserRouter>
