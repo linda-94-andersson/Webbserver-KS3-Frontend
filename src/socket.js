ed4f5be4a76128adb14a7f44f51705236ccd43aa
Linda Andersson <linda.29.andersson@gmail.com>
2022-08-24 10:03:18 +0200

    Testing out env url

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,10 @@
 import { io } from "socket.io-client";
 
 // const oldURL = "http://localhost:5000"
-const newURL = "https://webbpublicering-ks-backend.herokuapp.com"; 
+// const newURL = "https://webbpublicering-ks-backend.herokuapp.com"; 
+const envUrl = process.env.BACKEND_URL
 
-const socket = io(newURL, { autoConnect: false });
+const socket = io(envUrl, { autoConnect: false });
 
 socket.onAny((event, ...args) => {
   console.log(event, args);
