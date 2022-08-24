74e6674751e6303c07959c9ed194aa9a86ef21e2
Linda Andersson <linda.29.andersson@gmail.com>
2022-08-24 14:13:10 +0200

    Removed all env

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,9 @@
 import { io } from "socket.io-client";
 
 // const oldURL = "http://localhost:5000"
-// const newURL = "https://webbpublicering-ks-backend.herokuapp.com"; 
-const envUrl = process.env.BACKEND_URL
+const newURL = "https://webbpublicering-ks-backend.herokuapp.com"; 
 
-const socket = io(envUrl, { autoConnect: false });
+const socket = io(newURL, { autoConnect: false });
 
 socket.onAny((event, ...args) => {
   console.log(event, args);
